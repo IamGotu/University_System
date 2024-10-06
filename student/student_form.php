c96c7ecfde98fe4b08a97480e9af967875be64f6
Mark John <160160952+IamGotu@users.noreply.github.com>
2024-10-06 20:48:48 +0800

    student form

--- student/student_form.php
+++ student/student_form.php
@@ -5,8 +5,8 @@ session_start();
 // Include the database connection
 include("../database/db_connect.php");
 
-// Query to fetch course data
-$sql = "SELECT * FROM course";
+// Query to fetch student data
+$sql = "SELECT * FROM student";
 $result = $conn->query($sql);
 
 // Query to fetch department data (dept_name)
@@ -27,14 +27,14 @@ if ($result_departments->num_rows > 0) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Course</title>
+    <title>Student</title>
     <link rel="stylesheet" href="../resources/css/style.css">
 </head>
 <body>
     <?php include('../components/sidebar.php'); ?>
 
     <div class="main-content">
-        <h1>Course</h1>
+        <h1>Student</h1>
         
         <?php
             // Check for status and message
@@ -50,21 +50,27 @@ if ($result_departments->num_rows > 0) {
         ?>
         <br>
 
-        <!-- Button to Open the Modal for Adding Course -->
-        <button id="openModalBtn">Add New Course</button>
+        <!-- Button to Open the Modal for Adding Student -->
+        <button id="openModalBtn">Add New Student</button>
 
-        <!-- The Modal for Adding Course -->
+        <!-- The Modal for Adding Student -->
         <div id="myModal" class="modal">
             <div class="modal-content">
                 <span class="close">&times;</span>
-                <!-- Form to Add Course -->
-                <form method="POST" action="/course/course_function.php">
+                <!-- Form to Add Student -->
+                <form method="POST" action="/student/student_function.php">
                 <br>
-                    <label for="course_id">Course ID:</label>
-                    <input type="text" id="course_id" name="course_id" required><br><br>
+                    <label for="ID">Student ID:</label>
+                    <input type="text" id="ID" name="ID" required><br><br>
 
-                    <label for="title">Title:</label>
-                    <input type="text" id="title" name="title" required><br><br>
+                    <label for="first_name">First Name:</label>
+                    <input type="text" id="first_name" name="first_name" required><br><br>
+
+                    <label for="middle_name">Middle Name:</label>
+                    <input type="text" id="middle_name" name="middle_name" required><br><br>
+
+                    <label for="last_name">Last Name:</label>
+                    <input type="text" id="last_name" name="last_name" required><br><br>
 
                     <label for="dept_name">Department:</label>
                     <select id="dept_name" name="dept_name" required>
@@ -75,11 +81,11 @@ if ($result_departments->num_rows > 0) {
                     </select>
                     <br><br>
 
-                    <label for="credits">Credits:</label>
-                    <input type="text" id="credits" name="credits"><br><br>
+                    <label for="tot_cred">Total Credits:</label>
+                    <input type="text" id="tot_cred" name="tot_cred"><br><br>
 
                     <div class="button-container">
-                        <input type="submit" name="add_course" value="Add Course">
+                        <input type="submit" name="add_student" value="Add Student">
                         <button type="button" id="cancelAddBtn">Cancel</button>
                     </div>
                 </form>
@@ -88,14 +94,16 @@ if ($result_departments->num_rows > 0) {
 
         <br><br>
 
-        <!-- Display Courses -->
+        <!-- Display students -->
         <table style="width:100%">
             <thead>
                 <tr>
-                    <th>Course ID</th>
-                    <th>Title</th>
+                    <th>Student ID</th>
+                    <th>First Name</th>
+                    <th>Middle Name</th>
+                    <th>Last Name</th>
                     <th>Department</th>
-                    <th>Credits</th>
+                    <th>Total Credits</th>
                     <th>Actions</th>
                 </tr>
             </thead>
@@ -103,41 +111,49 @@ if ($result_departments->num_rows > 0) {
                 <?php if ($result->num_rows > 0) : ?>
                     <?php while ($row = $result->fetch_assoc()) : ?>
                         <tr>
-                            <td style="text-align: center;"><?php echo $row['course_id']; ?></td>    
-                            <td style="text-align: center;"><?php echo $row['title']; ?></td>
+                            <td style="text-align: center;"><?php echo $row['ID']; ?></td>    
+                            <td style="text-align: center;"><?php echo $row['first_name']; ?></td>
+                            <td style="text-align: center;"><?php echo $row['middle_name']; ?></td>
+                            <td style="text-align: center;"><?php echo $row['last_name']; ?></td>
                             <td style="text-align: center;"><?php echo $row['dept_name']; ?></td>
-                            <td style="text-align: center;"><?php echo $row['credits']; ?></td>
+                            <td style="text-align: center;"><?php echo $row['tot_cred']; ?></td>
                             <td>
                                 <!-- Edit and Delete buttons -->
                                 <div class="button-container">
-                                    <button class="edit-btn" data-course_id="<?php echo $row['course_id']; ?>" data-title="<?php echo $row['title']; ?>" data-dept_name="<?php echo $row['dept_name']; ?>" data-credits="<?php echo $row['credits']; ?>">Edit</button>
-                                    <button class="delete-btn" data-course_id="<?php echo $row['course_id']; ?>">Delete</button>
+                                    <button class="edit-btn" data-ID="<?php echo $row['ID']; ?>" data-first_name="<?php echo $row['first_name']; ?>" data-middle_name="<?php echo $row['middle_name']; ?>" data-last_name="<?php echo $row['last_name']; ?>" data-dept_name="<?php echo $row['dept_name']; ?>" data-tot_cred="<?php echo $row['tot_cred']; ?>">Edit</button>
+                                    <button class="delete-btn" data-ID="<?php echo $row['ID']; ?>">Delete</button>
                                 </div>
                             </td>
                         </tr>
                     <?php endwhile; ?>
                 <?php else : ?>
                     <tr>
-                        <td colspan="4">No Courses found</td>
+                        <td colspan="7">No students found</td>
                     </tr>
                 <?php endif; ?>
             </tbody>
         </table>
 
-        <!-- The Modal for Editing Course -->
+        <!-- The Modal for Editing Student -->
         <div id="editModal" class="modal">
             <div class="modal-content">
                 <span class="close">&times;</span>
-                <!-- Form to Edit Course -->
-                <form method="POST" action="/course/course_function.php">
+                <!-- Form to Edit Student -->
+                <form method="POST" action="/student/student_function.php">
                     <br>
-                    <input type="hidden" id="edit-course_id" name="course_id" required>
+                    <input type="hidden" id="edit-ID" name="ID" required>
 
-                    <label for="edit-new_course_id">Course ID:</label>
-                    <input type="text" id="edit-new_course_id" name="new_course_id" required><br><br>
+                    <label for="edit-new_ID">Student ID:</label>
+                    <input type="text" id="edit-new_ID" name="new_ID" required><br><br>
 
-                    <label for="edit-title">Title:</label>
-                    <input type="text" id="edit-title" name="title" required><br><br>
+                    <label for="edit-first_name">First Name:</label>
+                    <input type="text" id="edit-first_name" name="first_name" required><br><br>
+
+                    <label for="edit-middle_name">Middle Name:</label>
+                    <input type="text" id="edit-middle_name" name="middle_name" required><br><br>
+
+                    <label for="edit-last_name">Last Name:</label>
+                    <input type="text" id="edit-last_name" name="last_name" required><br><br>
 
                     <label for="edit-dept_name">Department:</label>
                     <select id="edit-dept_name" name="dept_name" required>
@@ -148,28 +164,28 @@ if ($result_departments->num_rows > 0) {
                     </select>
                     <br><br>
 
-                    <label for="edit-credits">Credits:</label>
-                    <input type="text" id="edit-credits" name="credits" required><br><br>
+                    <label for="edit-tot_cred">Total Credits:</label>
+                    <input type="text" id="edit-tot_cred" name="tot_cred" required><br><br>
 
                     <div class="button-container">
-                        <input type="submit" name="update_course" value="Update Course">
+                        <input type="submit" name="update_student" value="Update Student">
                         <button type="button" id="cancelEditBtn">Cancel</button>
                     </div>
                 </form>
             </div>
         </div>
 
-        <!-- The Modal for Deleting Course -->
+        <!-- The Modal for Deleting Student -->
         <div id="deleteModal" class="modal">
             <div class="modal-content">
                 <span class="close">&times;</span>
-                <!-- Confirmation for Deleting course -->
-                <p>Are you sure you want to delete this course?</p>
-                <form method="POST" action="/course/course_function.php">
-                    <input type="hidden" id="delete-course_id" name="course_id">
+                <!-- Confirmation for Deleting student -->
+                <p>Are you sure you want to delete this student?</p>
+                <form method="POST" action="/student/student_function.php">
+                    <input type="hidden" id="delete-ID" name="ID">
 
                     <div class="button-container">
-                        <input type="submit" name="delete_course" value="Delete Course">
+                        <input type="submit" name="delete_student" value="Delete Student">
                         <button type="button" id="cancelDeleteBtn">Cancel</button>
                     </div>
                 </form>
@@ -185,16 +201,20 @@ if ($result_departments->num_rows > 0) {
     var editButtons = document.getElementsByClassName("edit-btn");
     for (let i = 0; i < editButtons.length; i++) {
         editButtons[i].onclick = function() {
-            var course_id = this.getAttribute("data-course_id");
-            var title = this.getAttribute("data-title");
+            var ID = this.getAttribute("data-ID");
+            var first_name = this.getAttribute("data-first_name");
+            var middle_name = this.getAttribute("data-middle_name");
+            var last_name = this.getAttribute("data-last_name");
             var dept_name = this.getAttribute("data-dept_name");
-            var credits = this.getAttribute("data-credits");
+            var tot_cred = this.getAttribute("data-tot_cred");
 
-            document.getElementById("edit-course_id").value = course_id;
-            document.getElementById("edit-new_course_id").value = course_id;
-            document.getElementById("edit-title").value = title;
+            document.getElementById("edit-ID").value = ID;
+            document.getElementById("edit-new_ID").value = ID;
+            document.getElementById("edit-first_name").value = first_name;
+            document.getElementById("edit-middle_name").value = middle_name;
+            document.getElementById("edit-last_name").value = last_name;
             document.getElementById("edit-dept_name").value = dept_name;
-            document.getElementById("edit-credits").value = credits;
+            document.getElementById("edit-tot_cred").value = tot_cred;
 
             editModal.style.display = "block";
         }
@@ -204,9 +224,9 @@ if ($result_departments->num_rows > 0) {
     var deleteButtons = document.getElementsByClassName("delete-btn");
     for (let i = 0; i < deleteButtons.length; i++) {
         deleteButtons[i].onclick = function() {
-            var course_id = this.getAttribute("data-course_id");
+            var ID = this.getAttribute("data-ID");
 
-            document.getElementById("delete-course_id").value = course_id;
+            document.getElementById("delete-ID").value = ID;
 
             deleteModal.style.display = "block";
         }

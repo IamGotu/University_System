619ae833e2aade2963e1ae16fb19707586827de5
Mark John <160160952+IamGotu@users.noreply.github.com>
2024-10-06 18:48:50 +0800

    rearranging side bar and adding student folder

--- components/sidebar.php
+++ components/sidebar.php
@@ -2,10 +2,11 @@
 <div class="sidebar">
     <h2>University System</h2>
     <ul>
-        <li><a href="../instructor/instructor_form.php">Instructors</a></li>
+        <li><a href="../time-slot/time_slot_form.php">Time Slots</a></li>
+        <li><a href="../classroom/classroom_form.php">Classrooms</a></li>
         <li><a href="../department/department_form.php">Departments</a></li>
         <li><a href="../course/course_form.php">Courses</a></li>
-        <li><a href="../classroom/classroom_form.php">Classrooms</a></li>
-        <li><a href="../time-slot/time_slot.php">Time Slots</a></li>
+        <li><a href="../student/student_form.php">Students</a></li>
+        <li><a href="../instructor/instructor_form.php">Instructors</a></li>
     </ul>
 </div>

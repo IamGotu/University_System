ae5b63b9cb3910dc50844fc3acb97306f2b38328
Mark John <160160952+IamGotu@users.noreply.github.com>
2024-10-06 14:33:02 +0800

    updating redirections to the forms

--- components/sidebar.php
+++ components/sidebar.php
@@ -2,10 +2,10 @@
 <div class="sidebar">
     <h2>University System</h2>
     <ul>
-        <li><a href="instructor_form.php">Instructors</a></li>
+        <li><a href="../instructor/instructor_form.php">Instructors</a></li>
         <li><a href="../department/department_form.php">Departments</a></li>
-        <li><a href="course_form.php">Courses</a></li>
+        <li><a href="../course/course_form.php">Courses</a></li>
         <li><a href="../classroom/classroom_form.php">Classrooms</a></li>
-        <li><a href="time_slot_form.php">Time Slots</a></li>
+        <li><a href="../time-slot/time_slot.php">Time Slots</a></li>
     </ul>
 </div>

d3c66e851c21ea60f894a9b7bf4dc30db7fa28a3
Mark John <markjohnjopia1@gmail.com>
2024-10-31 02:35:40 +0800

    should be plural

--- student/student_form.php
+++ student/student_form.php
@@ -27,7 +27,7 @@ if ($result_departments->num_rows > 0) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student</title>
+    <title>Students</title>
     <link rel="stylesheet" href="../resources/css/style.css">
 </head>
 <body>
